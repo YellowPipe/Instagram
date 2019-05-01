474de2d51c0575204c41139203bcbb7439ac8694
YellowPipe <philippdmitrienko@gmail.com>
2019-05-01 14:50:16 +0300

    added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/show'
 devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-root to: 'sessions#new'
+root to: 'users#index'
 end
