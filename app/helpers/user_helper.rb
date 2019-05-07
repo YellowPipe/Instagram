f0417a8bd6c6236d0b3dc693b7622d6beaafe0c7
YellowPipe <philippdmitrienko@gmail.com>
2019-05-07 18:24:15 +0300

    gravatar for user show page

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,7 @@
 module UserHelper
+	def gravatar_for(user)
+	    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+	    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=150"
+	    image_tag(gravatar_url, alt: 'avatar', class: "rounded-circle img-fluid")
+  	end
 end
