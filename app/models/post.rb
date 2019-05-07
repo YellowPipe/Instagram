9435342173080c251eece13f46c3b7464865370c
YellowPipe <philippdmitrienko@gmail.com>
2019-05-07 17:13:52 +0300

    implemented user feed for followed users

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ApplicationRecord
 	belongs_to :user
 
 	mount_uploader :image, InstagramUploader
-
 	has_many :comments, dependent: :destroy
 	has_many :likes, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -42,4 +42,12 @@ class User < ApplicationRecord
   def liked?(post)
     liked_posts.include?(post)
   end
+
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
 end
