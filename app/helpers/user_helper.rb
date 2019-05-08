1fe8c143ee58ac3d185f006f56cd1b3c99473393
YellowPipe <philippdmitrienko@gmail.com>
2019-05-08 16:32:31 +0300

    improved index post view

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,7 +1,7 @@
 module UserHelper
-	def gravatar_for(user)
+	def gravatar_for(user, size: 150)
 	    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-	    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=150"
+	    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
 	    image_tag(gravatar_url, alt: 'avatar', class: "rounded-circle img-fluid")
   	end
 end
