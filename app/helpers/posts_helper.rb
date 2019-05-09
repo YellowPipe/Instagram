a6258e79718718e53ee3539a080dd6b434d32922
YellowPipe <philippdmitrienko@gmail.com>
2019-05-09 15:30:36 +0300

    post show page view

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+	def delete_button
+	    if policy(@post).destroy?
+	    	 button_to 'Delete Post', @post, method: :delete, data: { confirm: 'Are you sure?' }, class: 'btn btn-danger'
+	    end
+	end
+
+	def edit_button
+		if policy(@post).edit?
+	    	button_to 'Edit Post', edit_post_path(@post), method: :get, class: 'btn btn-warning'
+	    end
+	end
 end
